450f796145b3aae1cba0001f59d7b6ce40b779c1
Franco Peron <francomatiasperon@gmail.com>
2022-12-29 16:08:26 -0300

    ahora si que si

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -80,8 +80,6 @@ export default {};
 
 .main-contact {
   .main-contact__title {
-    @extend %f-monument;
-    @extend %f-bt1;
     font-weight: 500;
     color: $cWhite;
 
@@ -93,14 +91,14 @@ export default {};
 
     &::before {
       content: "-";
-      @extend %f-bt1;
+      
       color: $cWhite;
       margin-right: 3rem;
     }
 
     &::after {
       content: "-";
-      @extend %f-bt1;
+    
       color: $cWhite;
       margin-left: 3rem;
     }
@@ -120,8 +118,7 @@ export default {};
 }
 
 .contact_title {
-  @extend %f-title2;
-  @extend %f-t2;
+ 
   color: $cWhite;
   margin: 2rem;
 }
